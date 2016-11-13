c0909cf08c010ce8781f7832504a9ae97d91b183
antoine.derengerve <antoine.derengerve@partnering.fr>
2016-11-13 22:36:21 +0100

    Allow use of sensor-base with template dom-repeat. Backward compatibility is preserved. NB: precision is now computed automatically from resolution.

--- sensor-base.js
+++ sensor-base.js
@@ -16,11 +16,6 @@ DiyaBehaviors.SensorBase = {
 			value: 1,
 			notify: true
 		},
-		precision: {
-			type: Number,
-			value: 0,
-			notify: true
-		},
 		databind: { // data binding between sensors, fill with data from d1.IEQ().watch()
 			notify: true,
 			observer : 'onDataChanged'
@@ -34,8 +29,8 @@ DiyaBehaviors.SensorBase = {
 		if(!this.databind)
 			return;
 
-		var data = this.databind[this.name];
-		// console.log(data);
+		var data = this.databind[this.name] || this.databind; // select by name or give directly correct object
+		this.name = this.name || data.id;
 
 		// check data format
 		if (!data || !data.avg || !Array.isArray(data.avg.d) || isNaN(data.avg.d[0]) || !Array.isArray(data.avg.i) || isNaN(data.avg.i[0]) || !Array.isArray(data.range) || isNaN(data.range[0]) || isNaN(data.range[1])) {
@@ -46,9 +41,13 @@ DiyaBehaviors.SensorBase = {
 		this.min = data.range[0];
 		this.max = data.range[1];
 		var val = data.avg.d[0];
+		this.resolution = this.resolution || data.precision;
 		if(this.resolution>0)
 			val = Math.round(val/this.resolution)*this.resolution;
-		this.value = val.toFixed(this.precision);
-		// console.log(this.value);
+		var precision = -Math.ceil(Math.log10(this.resolution));
+		precision = (precision>0?precision:0);
+		this.value = val.toFixed(precision);
+
+		this.label = this.label || (LANG && LANG[this.name.toLowerCase()+"_label"]) || data.label;
 	}
 };
